d78feef59a71ac7d34a5447c42d369d8ee8bf70b
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-06-17 21:11:12 -0300

    feat: implementing mobile and desktop navbar

---
+++ components/sidebar/nav/desktop-nav.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useNavigation } from "@/hooks/use-navigation";
+import { UserButton } from "@clerk/clerk-react";
+import Link from "next/link";
+import React from "react";
+
+const DesktopNav = () => {
+  const paths = useNavigation();
+  return (
+    <Card className="hidden lg:flex lg:flex-col lg:justify-between lg:items-center lg:h-full lg:w-16 lg:px-2 lg:py-4">
+      <nav>
+        <ul className="flex flex-col items-center gap-4">
+          {paths.map((path, id) => {
+            return (
+              <li key={id} className="relative">
+                <Link href={path.href}>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <Button
+                        size="icon"
+                        variant={path.active ? "default" : "outline"}
+                      >
+                        {path.icon}
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{path.name}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <div className="flex flex-col gap-4 items-center">
+        <UserButton />
+      </div>
+    </Card>
+  );
+};
+
+export default DesktopNav;
--- components/sidebar/sidebar-wraper.tsx
+++ components/sidebar/sidebar-wraper.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import DesktopNav from "./nav/desktop-nav";
+import MobileNav from "./nav/mobile-nav";
 
 type Props = React.PropsWithChildren<{}>;
 
 const SidebarWrapper = ({ children }: Props) => {
   return (
     <div className="h-full w-full p-4 flex flex-col lg:flex-row gap-4">
+      <MobileNav />
+      <DesktopNav />
       <main className="h-[calc(100%-80px)] lg:h-full w-full flex gap-4">
         {children}
       </main>
